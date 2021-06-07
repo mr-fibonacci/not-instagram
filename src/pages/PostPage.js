020786e0004a52a24398494ff266c9b3c608077f
mr-fibonacci <adamlapinski1@gmail.com>
2021-06-07 19:17:08 +0000

    implemented lazy loading for comments

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -5,12 +5,13 @@ import CommentCreateForm from "../components/CommentCreateForm";
 import Button from "react-bootstrap/Button";
 import { useHistory, useParams } from "react-router";
 import Post from "../components/Post";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function PostPage(props) {
   const { id } = useParams();
   const history = useHistory();
   const [[post], setPost] = useState([{}]);
-  const [comments, setComments] = useState({});
+  const [comments, setComments] = useState({ count: 0, results: [] });
   useEffect(() => {
     handleMount();
   }, []);
@@ -37,6 +38,25 @@ function PostPage(props) {
     }
   };
 
+  const fetchMoreComments = async () => {
+    try {
+      const { data } = await axios.get(comments.next);
+      setComments((prevComments) => ({
+        ...data,
+        next: data.next
+          ? data.next.replace("http://localhost:8000", axios.defaults.baseURL)
+          : null,
+        results: data.results.reduce((acc, cur) => {
+          return acc.some((result) => result.id === cur.id)
+            ? acc
+            : [...acc, cur];
+        }, prevComments.results),
+      }));
+    } catch (err) {
+      console.log(err.request);
+    }
+  };
+
   return (
     <>
       <Post {...post} setPosts={setPost} />
@@ -49,14 +69,23 @@ function PostPage(props) {
         setPost={setPost}
         setComments={setComments}
       />
-      {comments.results?.map((comment) => (
-        <Comment
-          key={comment.id}
-          setPost={setPost}
-          {...comment}
-          setComments={setComments}
-        />
-      ))}
+      <InfiniteScroll
+        dataLength={comments.results.length}
+        next={fetchMoreComments}
+        hasMore={!!comments.next}
+        loader={<h4>loading</h4>}
+        endMessage={<p>everything has been loaded</p>}
+        scrollableTarget="scrollable-div"
+      >
+        {comments.results?.map((comment) => (
+          <Comment
+            key={comment.id}
+            setPost={setPost}
+            {...comment}
+            setComments={setComments}
+          />
+        ))}
+      </InfiniteScroll>
     </>
   );
 }
