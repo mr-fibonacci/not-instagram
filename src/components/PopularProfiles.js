901c27345d46c17888293f2785d975be0f652a54
mr-fibonacci <adamlapinski1@gmail.com>
2021-08-29 21:32:09 +0000

    Got PopularProfiles working on PostsPages

--- src/components/PopularProfiles.js
+++ src/components/PopularProfiles.js
@@ -3,11 +3,61 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Profile from "./Profile";
 import styles from "../App.module.css";
-import { usePopularProfilesContext } from "../PopularProfilesContext";
+import {
+  usePopularProfilesContext,
+  useSetPopularProfilesContext,
+} from "../PopularProfilesContext";
 
 const PopularProfiles = ({ handleFollow, handleUnfollow }) => {
   const popularProfiles = usePopularProfilesContext();
+  const setPopularProfilesContext = useSetPopularProfilesContext();
   console.log("popular profiles", popularProfiles);
+
+  const follow = async (clickedProfile) => {
+    try {
+      const { data } = await axios.post("/followers/", {
+        followed: clickedProfile.id,
+      });
+      setPopularProfilesContext((prevProfiles) => ({
+        ...prevProfiles,
+        results: prevProfiles.results.map((profile) => {
+          return profile.id === clickedProfile.id
+            ? {
+                ...profile,
+                followers_count: profile.followers_count + 1,
+                following_id: data.id,
+              }
+            : profile.is_owner
+            ? { ...profile, following_count: profile.following_count + 1 }
+            : profile;
+        }),
+      }));
+    } catch (err) {
+      console.log(err.request);
+    }
+  };
+  const unfollow = async (clickedProfile) => {
+    try {
+      await axios.delete(`/followers/${clickedProfile.following_id}/`);
+      setPopularProfilesContext((prevProfiles) => ({
+        ...prevProfiles,
+        results: prevProfiles.results.map((profile) => {
+          // const is_owner = currentUser?.username === profile.owner;
+          return profile.id === clickedProfile.id
+            ? {
+                ...profile,
+                followers_count: profile.followers_count - 1,
+                following_id: null,
+              }
+            : profile.is_owner
+            ? { ...profile, following_count: profile.following_count - 1 }
+            : profile;
+        }),
+      }));
+    } catch (err) {
+      console.log(err.request);
+    }
+  };
   return (
     <Container className={styles.Content}>
       <p> you may also like...</p>
@@ -16,8 +66,8 @@ const PopularProfiles = ({ handleFollow, handleUnfollow }) => {
           key={profile.id}
           profile={profile}
           stats={false}
-          handleFollow={handleFollow}
-          handleUnfollow={handleUnfollow}
+          handleFollow={handleFollow || follow}
+          handleUnfollow={handleUnfollow || unfollow}
         />
       ))}
     </Container>
